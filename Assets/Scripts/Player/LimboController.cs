c16a1467c56e19f6a2933463102747dce58375f8
GabrielBeltz <74629809+GabrielBeltz@users.noreply.github.com>
2021-11-28 12:09:15 -0400

    Implementação do Timer da 2ª dimensão.

--- Assets/Scripts/Player/LimboController.cs
+++ Assets/Scripts/Player/LimboController.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class LimboController : MonoBehaviour
 {
     public bool IsInLimboMode = false;
-    private float t = 0;
+    public float limboDurationMod, limboTimer;
+    private float limboDuration = 3f;
     [SerializeField] private GameObject SpriteMaksObj;
-    Coroutine limboGeneration;
 
     void Start()
     {
@@ -17,47 +14,28 @@ public class LimboController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.L))
+        if(Input.GetKeyDown(KeyCode.L) && limboTimer < limboDuration + limboDurationMod - 0.5f)
         {
-            IsInLimboMode = !IsInLimboMode;
+            LimboMode();
         }
 
-        LimboMode();
+        if (IsInLimboMode)
+        {
+            limboTimer += Time.deltaTime;
+            if (limboTimer > limboDuration + limboDurationMod)
+            {
+                LimboMode();
+            }
+        }
+        else
+        {
+            limboTimer = Mathf.Max(limboTimer - Time.deltaTime, 0);
+        }
     }
 
     private void LimboMode()
     {
-        if (limboGeneration != null)
-        {
-            StopCoroutine(limboGeneration);
-        }
-
-        limboGeneration = StartCoroutine(GeneratingMask());
-    }
-
-    private IEnumerator GeneratingMask()
-    {
-        switch (IsInLimboMode)
-        {
-            case true:
-                SpriteMaksObj.SetActive(true);
-                while (!(t > 1))
-                {
-                    yield return new WaitForEndOfFrame();
-                    t += Time.deltaTime;
-                    SpriteMaksObj.transform.localScale = new Vector3(Mathf.Lerp(0.5f, 800, t / 1f), Mathf.Lerp(0.5f, 800, t / 1f), 0);
-                }
-                break;
-            case false:
-                while (!(t < 0))
-                {
-                    yield return new WaitForEndOfFrame();
-                    t -= Time.deltaTime * 3;
-                    SpriteMaksObj.transform.localScale = new Vector3(Mathf.Lerp(0.5f, 800, t / 1f), Mathf.Lerp(0.5f, 800, t / 1f), 0);
-                }
-                SpriteMaksObj.SetActive(false);
-                break;
-        }
-
+        IsInLimboMode = !IsInLimboMode;
+        SpriteMaksObj.SetActive(IsInLimboMode);
     }
 }
