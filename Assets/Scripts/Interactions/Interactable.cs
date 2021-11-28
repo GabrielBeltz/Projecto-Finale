b7f1a6358d9ead39749c6039830bb486717b9ec9
GabrielBeltz <74629809+GabrielBeltz@users.noreply.github.com>
2021-11-27 21:41:17 -0400

    Desbloqueio de Interactables

--- Assets/Scripts/Interactions/Interactable.cs
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -14,26 +12,46 @@ public class Interactable : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            onExitInteraction?.Invoke();
+             onExitInteraction?.Invoke();
         }
     }
 
     public virtual void Interact()
     {
+        int overrideIndex = 0;
+        bool overrideInteraction = false;
+        for (int i = 0; i < interactions.Length; i++)
+        {
+            if (interactions[i].getKeys.Length > 0)
+            {
+                for (int j = 0; j < interactions[i].getKeys.Length; j++)
+                {
+                    overrideInteraction = InteractionManager.Instance.GetKey(interactions[i].getKeys[j]);
+                    overrideIndex = i;
+                }
+            }
+        }
+
+        if (overrideInteraction)
+        {
+            interactionIndex = overrideIndex;
+        }
+
         for (int j = 0; j < interactions[interactionIndex].events.Length; j++)
         {
             interactions[interactionIndex].events[j]?.Invoke();
         }
+
+        if (interactions[interactionIndex].setKeys.Length > 0)
+        {
+            for (int i = 0; i < interactions[interactionIndex].setKeys.Length; i++)
+            {
+                InteractionManager.Instance.SetKey(interactions[interactionIndex].setKeys[i]);
+            }
+        }
+
         OnInteract?.Invoke();
 
-        interactionIndex = Mathf.Min(interactionIndex + 1, interactions.Length - 1);
+        interactionIndex = Mathf.Min(interactionIndex + interactions[interactionIndex].indexMod, interactions.Length - 1);
     }
-
-
-}
-
-[System.Serializable]
-public class Interaction
-{
-    public UnityEvent[] events;
 }
---
+++ Assets/Scripts/Interactions/InteractionManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractionManager : MonoBehaviour
+{
+    static public InteractionManager Instance;
+
+    private void Start()
+    {
+        if (InteractionManager.Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public List<InteractionKey> keys;
+
+    public bool GetKey(InteractionKey interactionKey)
+    {
+        return keys.Find(key => key.name == interactionKey.name).value == interactionKey.value;
+    }
+
+    public void SetKey(InteractionKey interactionKey)
+    {
+        keys.Find(key => key.name == interactionKey.name).value = interactionKey.value;
+    }
+}
+
+[System.Serializable]
+public class Interaction
+{
+    public int indexMod;
+    public UnityEvent[] events;
+    // keys que desbloqueiam essa interação. por enquanto elas tem somente comportamento OR,
+    // qualquer uma delas que retornar como verdadeiras desbloqueia a interação
+    public InteractionKey[] getKeys;  
+    // keys setadas na interação, para desbloquear outras interações
+    public InteractionKey[] setKeys;
+}
+
+[System.Serializable]
+public class InteractionKey
+{
+    public string name;
+    public bool value;
+}
