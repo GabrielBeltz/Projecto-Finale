1d2ae34f69e841cefb7d5c738fbd5cadc3c15992
Renanz3ra <69330107+Renanz3ra@users.noreply.github.com>
2021-12-02 16:40:38 -0300

    Ajustes
    Alguns glitches que ocorriam no cenario foram ajustados, mobs que passavam paredes ou ignoravam colisões foram ajustados

--- Assets/Scripts/Interactions/CommonInteractionsEvents.cs
+++ Assets/Scripts/Interactions/CommonInteractionsEvents.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 // Classe criada pra evitar referências diretas de GameObjects que não existem em algumas cenas mas são marcados como DontDestroyOnLoad
 public class CommonInteractionsEvents : MonoBehaviour
 {
+ 
+   // public void LoadScene(string sceneName)
+    //{ SceneTransitionManager.Instance.LoadScene(sceneName);  }
+
     public void LoadScene(string sceneName)
     {
-        SceneTransitionManager.Instance.LoadScene(sceneName);
+       SceneManager.LoadScene(sceneName);
     }
 
     public void SetKeyTrue(string keyName)
