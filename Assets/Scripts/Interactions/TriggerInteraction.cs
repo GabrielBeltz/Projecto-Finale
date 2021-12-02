ea285ea02d934cc40b8c80d825cc8c475f6ade70
GabrielBeltz <74629809+GabrielBeltz@users.noreply.github.com>
2021-12-01 21:05:24 -0400

    Transição Area 1/2

---
+++ Assets/Scripts/Interactions/TriggerInteraction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TriggerInteraction : MonoBehaviour
+{
+    public Interaction interaction;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            interaction.events?.Invoke();
+        }
+    }
+}
