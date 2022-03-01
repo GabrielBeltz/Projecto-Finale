78270b992001375416ea339cbd3ea38a3c1628a1
GabrielBeltz <74629809+GabrielBeltz@users.noreply.github.com>
2022-03-01 02:58:37 -0300

    Aprimoramentos de Interações

--- Assets/Scripts/Interactions/Interactable.cs
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -3,52 +3,75 @@ using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
-    public int interactionIndex;
-    public Interaction[] interactions;
+    public int InteractionIndex = 0;
+    public Interaction[] Interactions;
     public UnityAction OnInteract;
-    public UnityEvent onExitInteraction;
+    public UnityEvent OnExitInteraction;
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if(Interactions.Length == 0)
         {
-             onExitInteraction?.Invoke();
+            Debug.LogWarning($"Interações não setadas. [Clique aqui para achar o objeto na hierarquia]", this);
+            enabled = false;
         }
     }
 
     public virtual void Interact()
     {
-        int overrideIndex = 0;
-        bool overrideInteraction = false;
-        for (int i = 0; i < interactions.Length; i++)
-        {
-            if (interactions[i].getKeys.Length > 0)
-            {
-                for (int j = 0; j < interactions[i].getKeys.Length; j++)
-                {
-                    overrideInteraction = InteractionManager.Instance.GetKey(interactions[i].getKeys[j]);
-                    overrideIndex = i;
-                }
-            }
+        CheckOverridingInteraction();
+
+        if(Interactions[InteractionIndex].indexMod == 0)
+        { 
+            OnExitInteraction?.Invoke();
+            return;
         }
 
-        if (overrideInteraction)
-        {
-            interactionIndex = overrideIndex;
-        }
+        Interactions[InteractionIndex].events?.Invoke();
 
-        interactions[interactionIndex].events?.Invoke();
-
-        if (interactions[interactionIndex].setKeys.Length > 0)
+        if (Interactions[InteractionIndex].setKeys.Length > 0)
         {
-            for (int i = 0; i < interactions[interactionIndex].setKeys.Length; i++)
+            for (int i = 0; i < Interactions[InteractionIndex].setKeys.Length; i++)
             {
-                InteractionManager.Instance.SetKey(interactions[interactionIndex].setKeys[i]);
+                InteractionManager.Instance.SetKey(Interactions[InteractionIndex].setKeys[i]);
             }
         }
 
         OnInteract?.Invoke();
 
-        interactionIndex = Mathf.Min(interactionIndex + interactions[interactionIndex].indexMod, interactions.Length - 1);
+        InteractionIndex = Mathf.Min(InteractionIndex + Interactions[InteractionIndex].indexMod, Interactions.Length - 1);
+    }
+
+    void CheckOverridingInteraction()
+    {
+        int overrideIndex = 0;
+        bool overrideInteraction = false;
+        for (int i = 0; i < Interactions.Length; i++)
+        {
+            if (Interactions[i].getKeys.Length > 0)
+            {
+                switch(Interactions[i].getKeysMode)
+                {
+                    case Interaction.InteractionKeysMode.OR:
+                        for (int j = 0; j < Interactions[i].getKeys.Length; j++)
+                        {
+                            overrideInteraction = InteractionManager.Instance.GetKey(Interactions[i].getKeys[j]);
+                            overrideIndex = i;
+                        }
+                        break;
+                    case Interaction.InteractionKeysMode.AND:
+                        overrideInteraction = true;
+                        for (int j = 0; j < Interactions[i].getKeys.Length; j++)
+                        {
+                            overrideInteraction &= InteractionManager.Instance.GetKey(Interactions[i].getKeys[j]);
+                            overrideIndex = i;
+                        }
+                        break;
+                }
+                
+            }
+        }
+
+        InteractionIndex = overrideInteraction? overrideIndex : InteractionIndex;
     }
 }
--- Assets/Scripts/Interactions/InteractionManager.cs
+++ Assets/Scripts/Interactions/InteractionManager.cs
@@ -6,14 +6,13 @@ using UnityEngine.Events;
 public class InteractionManager : MonoBehaviour
 {
     static public InteractionManager Instance;
-    // Carrega a lista de keys do scriptable object
     public List<InteractionKey> loadedKeys;
     [Header("Needed to Work")]
     public KeysList keyList;
 
     private void Start()
     {
-        if (InteractionManager.Instance != null)
+        if (Instance != null)
         {
             Destroy(this);
         }
@@ -22,37 +21,30 @@ public class InteractionManager : MonoBehaviour
             Instance = this;
         }
 
-        GameObject.DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
         loadedKeys = keyList.keys;
     }
 
-    public bool GetKey(InteractionKey interactionKey)
-    {
-        return loadedKeys.Find(key => key.name == interactionKey.name).value == interactionKey.value;
-    }
+    public bool GetKey(InteractionKey interactionKey) => loadedKeys.Find(key => key.name == interactionKey.name).value == interactionKey.value;
 
-    public bool GetKey(string keyName)
-    {
-        return loadedKeys.Find(key => key.name == keyName).value;
-    }
+    public bool GetKey(string keyName) => loadedKeys.Find(key => key.name == keyName).value;
 
-    public void SetKey(InteractionKey interactionKey)
-    {
-        loadedKeys.Find(key => key.name == interactionKey.name).value = interactionKey.value;
-    }
+    public void SetKey(InteractionKey interactionKey) => loadedKeys.Find(key => key.name == interactionKey.name).value = interactionKey.value;
 }
 
 [System.Serializable]
 public class Interaction
 {
-    // Por padrão deixei 1, mas deixando em 0 no editor impede a interação de automaticamente passar para a próxima
-    public int indexMod = 1;
+    public int indexMod = 1; // Padrão é 1, deixando em 0 no editor impede a interação de automaticamente passar para a próxima
     public UnityEvent events;
-    // keys que desbloqueiam essa interação. por enquanto elas tem somente comportamento OR,
-    // qualquer uma delas que retornar como verdadeiras desbloqueia a interação
-    public InteractionKey[] getKeys;  
-    // keys setadas na interação, para desbloquear outras interações
-    public InteractionKey[] setKeys;
+    public InteractionKeysMode getKeysMode; // se setadas para "or" qualquer uma das keys desbloqueiam, se for "and" é necessário todas as keys
+    public InteractionKey[] getKeys; // keys que desbloqueiam essa interação. 
+    public InteractionKey[] setKeys; // keys setadas na interação, para desbloquear outras interações
+
+    public enum InteractionKeysMode
+    {
+        OR, AND
+    }
 }
 
 [System.Serializable]
