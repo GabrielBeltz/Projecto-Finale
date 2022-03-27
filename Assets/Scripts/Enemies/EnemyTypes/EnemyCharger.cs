59313aa6c5150f5eb594c02468a225ff6bdfa189
GabrielBeltz <74629809+GabrielBeltz@users.noreply.github.com>
2022-03-27 12:39:44 -0300

    Enemy Charger hotfix

--- Assets/Scripts/Enemies/EnemyTypes/EnemyCharger.cs
+++ Assets/Scripts/Enemies/EnemyTypes/EnemyCharger.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 [RequireComponent(typeof(EnemyPatrolWalls))]
 public class EnemyCharger : MonoBehaviour
 {
     [Header("Configs")]
     public float SeeingPlayerSpeedMultiplier = 1.5f;
-    public float HeightDistanceToSeePlayer = 2;
-    public float WidthDistanceToSeePlayer = 7.5f;
+    public float HeightDistanceToSeePlayer = 2, WidthDistanceToSeePlayer = 7.5f;
     [Header("Live Feedback")]
     [SerializeField] bool PreparingAttack;
     [SerializeField] bool Attacking;
@@ -15,7 +13,6 @@ public class EnemyCharger : MonoBehaviour
     [SerializeField] string PlayerTag;
     PlayerController _player;
     EnemyPatrolWalls _patrolScript;
-
     float originalSpeed;
 
     private void Awake()
@@ -28,29 +25,22 @@ public class EnemyCharger : MonoBehaviour
 
     void Update()
     {
-        if(CheckPlayerPosition())
-        {
-            _patrolScript.speed = originalSpeed * SeeingPlayerSpeedMultiplier  * Mathf.Sign(_patrolScript.speed);
-            LookAtPlayer();
-        }
-        else
-        {
-            _patrolScript.speed = originalSpeed * Mathf.Sign(_patrolScript.speed);
-        }
+        bool playerHere = CheckPlayerPosition();
+        if(playerHere) LookAtPlayer();
+        
+        _patrolScript.speed = playerHere ? originalSpeed * SeeingPlayerSpeedMultiplier  * Mathf.Sign(_patrolScript.speed) : originalSpeed * Mathf.Sign(_patrolScript.speed);
     }
     
     bool CheckPlayerPosition()
     {
         Collider2D[] raycastHit2Ds = Physics2D.OverlapCircleAll(transform.position, WidthDistanceToSeePlayer, ~2);
-        if(raycastHit2Ds.Length > 0)
+        if(raycastHit2Ds.Length < 1) return false;
+        foreach(var hit in raycastHit2Ds)
         {
-            foreach(var hit in raycastHit2Ds)
+            if(hit.gameObject.CompareTag(PlayerTag)) 
             {
-                if(hit.gameObject.CompareTag(PlayerTag)) 
-                {
-                    if(hit.transform.position.y - HeightDistanceToSeePlayer < transform.position.y && hit.transform.position.y + HeightDistanceToSeePlayer > transform.position.y) return true;
-                    else if(hit.transform.position.y + HeightDistanceToSeePlayer > transform.position.y && hit.transform.position.y - HeightDistanceToSeePlayer < transform.position.y) return true;
-                }
+                if(hit.transform.position.y - HeightDistanceToSeePlayer < transform.position.y && hit.transform.position.y + HeightDistanceToSeePlayer > transform.position.y) return true;
+                else if(hit.transform.position.y + HeightDistanceToSeePlayer > transform.position.y && hit.transform.position.y - HeightDistanceToSeePlayer < transform.position.y) return true;
             }
         }
 
@@ -63,6 +53,5 @@ public class EnemyCharger : MonoBehaviour
         bool lookingAtRight = transform.localScale.x > 0;
 
         if(playerToTheRight != lookingAtRight) _patrolScript.Backtrack();
-
     }
 }
