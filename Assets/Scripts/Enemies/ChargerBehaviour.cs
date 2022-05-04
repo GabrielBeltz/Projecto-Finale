edb035b330ec89c22dbceb8b79ce257ae6a4b8bb
Henrique <henriquebarbisan@gmail.com>
2022-05-04 00:14:26 -0300

    Shield Enemy Update

--- Assets/Scripts/Enemies/ChargerBehaviour.cs
+++ Assets/Scripts/Enemies/ChargerBehaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ChargerBehaviour : MonoBehaviour
 {
+    public enum State { Patrol, Attack };
+    public State currentState;
     public Transform frontFeet;
     public ContactFilter2D contactFilter2D;
     public LayerMask wantedLayer;
@@ -11,43 +13,73 @@ public class ChargerBehaviour : MonoBehaviour
     private BoxCollider2D _myPatrolAreaCollider;
     public float moveSpeed, hitDistance;
     private float _modifiedSpeed;
+    private Transform _player;
     RaycastHit2D[] hit = new RaycastHit2D[1];
     [SerializeField] GameObject _groundArea;
-    [SerializeField] private bool isPlayerInMyPatrolArea = false;
+    [SerializeField] private bool isPlayerInMyPatrolArea;
+
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-
         Invoke("GetGroundObject", 0.3f);
+        isPlayerInMyPatrolArea = false;
+        currentState = State.Patrol;
+        _player = GameObject.Find("Player").GetComponent<Transform>();
     }
 
     private void Update()
     {
-        IsPlayerInMyPatrolArea();
         isPlayerInMyPatrolArea = IsPlayerInMyPatrolArea();
     }
 
     private void FixedUpdate()
     {
-        _modifiedSpeed = moveSpeed * Mathf.Sign(transform.localScale.x);
+        switch(currentState)
+        {
+            case State.Patrol:
+                Patroling();
+                break;
+
+            case State.Attack:
+                Attack();
+                break;
+        }
+    }
+
+    private void Patroling()
+    {
+        if(!isPlayerInMyPatrolArea)
+        {
+            _modifiedSpeed = moveSpeed * Mathf.Sign(transform.localScale.x);
+            _rb.velocity = new Vector2(_modifiedSpeed, 0f);
+
+            if(Physics2D.Raycast(frontFeet.position, frontFeet.right, contactFilter2D, hit, hitDistance) > Mathf.Epsilon || Physics2D.Raycast(frontFeet.position, -Vector3.up, contactFilter2D, hit, hitDistance * 3f) == 0f)
+            {
+                RotateAll();
+            }
+        }
+        else
+        {
+            currentState = State.Attack;
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+
+        bool playerToTheRight = _player.position.x > transform.position.x ? true : false;
+
+        if(playerToTheRight != IsFacingRight())
+        {
+            RotateAll();
+        }
+
+        _modifiedSpeed = moveSpeed * Mathf.Sign(transform.localScale.x) * 1.5f;
         _rb.velocity = new Vector2(_modifiedSpeed, 0f);
 
-        if(Physics2D.Raycast(frontFeet.position, frontFeet.right, contactFilter2D, hit, hitDistance) > Mathf.Epsilon || Physics2D.Raycast(frontFeet.position, -Vector3.up, contactFilter2D, hit, hitDistance * 3f) == 0f)
-        {
-            if(IsFacingRight())
-            {
-                //Move Right
-                frontFeet.rotation = Quaternion.Euler(new Vector3(0f, 0f, -180f));
-            }
-            else
-            {
-                //Move Left
-                frontFeet.rotation = Quaternion.Euler(new Vector3(0f, 0f, -0f));
-            }
-
-            Rotate();
-        }
+        // Não finalizado
     }
 
     private void GetGroundObject()
@@ -74,6 +106,22 @@ public class ChargerBehaviour : MonoBehaviour
     private void Rotate()
     {
         this.transform.localScale = new Vector2(-Mathf.Sign(_rb.velocity.x), this.transform.localScale.y);
+    }
+
+    private void RotateAll()
+    {
+        if(IsFacingRight())
+        {
+            //Move Right
+            frontFeet.rotation = Quaternion.Euler(new Vector3(0f, 0f, -180f));
+        }
+        else
+        {
+            //Move Left
+            frontFeet.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+        }
+
+        Rotate();
     }
 
     private void OnDrawGizmos()
