d62af8d77174927039c170d60eafe86a97eeb33e
Henrique <henriquebarbisan@gmail.com>
2022-05-04 22:03:23 -0300

    Shield Enemy Movemente Complete

--- Assets/Scripts/Enemies/ChargerBehaviour.cs
+++ Assets/Scripts/Enemies/ChargerBehaviour.cs
@@ -9,28 +9,28 @@ public class ChargerBehaviour : MonoBehaviour
     public Transform frontFeet;
     public ContactFilter2D contactFilter2D;
     public LayerMask wantedLayer;
-    [SerializeField] private Rigidbody2D _rb;
-    private BoxCollider2D _myPatrolAreaCollider;
-    public float moveSpeed, hitDistance;
-    private float _modifiedSpeed;
     private Transform _player;
-    RaycastHit2D[] hit = new RaycastHit2D[1];
-    [SerializeField] GameObject _groundArea;
-    [SerializeField] private bool isPlayerInMyPatrolArea;
+    private RaycastHit2D[] _hit = new RaycastHit2D[1];
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _moveSpeed, _hitDistance, _modifiedSpeed;
+    [SerializeField] private GameObject _groundArea;
+    [SerializeField] private bool _isPlayerInMyPatrolArea, _getPlayerPos, _finishAttackMode;
 
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         Invoke("GetGroundObject", 0.3f);
-        isPlayerInMyPatrolArea = false;
-        currentState = State.Patrol;
         _player = GameObject.Find("Player").GetComponent<Transform>();
+        _isPlayerInMyPatrolArea = false;
+        _finishAttackMode = true;
+        _getPlayerPos = false;
+        currentState = State.Patrol;
     }
 
     private void Update()
     {
-        isPlayerInMyPatrolArea = IsPlayerInMyPatrolArea();
+        _isPlayerInMyPatrolArea = IsPlayerInMyPatrolArea();
     }
 
     private void FixedUpdate()
@@ -49,12 +49,15 @@ public class ChargerBehaviour : MonoBehaviour
 
     private void Patroling()
     {
-        if(!isPlayerInMyPatrolArea)
+        _getPlayerPos = false;
+        _finishAttackMode = false;
+
+        if(!_isPlayerInMyPatrolArea)
         {
-            _modifiedSpeed = moveSpeed * Mathf.Sign(transform.localScale.x);
+            _modifiedSpeed = _moveSpeed * Mathf.Sign(transform.localScale.x);
             _rb.velocity = new Vector2(_modifiedSpeed, 0f);
 
-            if(Physics2D.Raycast(frontFeet.position, frontFeet.right, contactFilter2D, hit, hitDistance) > Mathf.Epsilon || Physics2D.Raycast(frontFeet.position, -Vector3.up, contactFilter2D, hit, hitDistance * 3f) == 0f)
+            if(Physics2D.Raycast(frontFeet.position, frontFeet.right, contactFilter2D, _hit, _hitDistance) > Mathf.Epsilon || Physics2D.Raycast(frontFeet.position, -Vector3.up, contactFilter2D, _hit, _hitDistance * 3f) == 0f)
             {
                 RotateAll();
             }
@@ -67,19 +70,31 @@ public class ChargerBehaviour : MonoBehaviour
 
     private void Attack()
     {
-        Debug.Log("Attack");
-
-        bool playerToTheRight = _player.position.x > transform.position.x ? true : false;
-
-        if(playerToTheRight != IsFacingRight())
+        if(!_getPlayerPos)
         {
-            RotateAll();
+            bool playerToTheRight = _player.position.x > transform.position.x ? true : false;
+
+            if(playerToTheRight != IsFacingRight())
+            {
+                RotateAll();
+            }
+            _getPlayerPos = true;
         }
 
-        _modifiedSpeed = moveSpeed * Mathf.Sign(transform.localScale.x) * 1.5f;
-        _rb.velocity = new Vector2(_modifiedSpeed, 0f);
-
-        // Não finalizado
+        if(!_finishAttackMode)
+        {
+            _modifiedSpeed = _moveSpeed * Mathf.Sign(transform.localScale.x) * 3f;
+            _rb.velocity = new Vector2(_modifiedSpeed, 0f);
+            
+            if(Physics2D.Raycast(frontFeet.position, frontFeet.right, contactFilter2D, _hit, _hitDistance) > Mathf.Epsilon || Physics2D.Raycast(frontFeet.position, -Vector3.up, contactFilter2D, _hit, _hitDistance * 3f) == 0f)
+            {
+                _finishAttackMode = true;
+            }
+        }
+        else
+        {
+            currentState = State.Patrol;
+        }
     }
 
     private void GetGroundObject()
@@ -127,7 +142,7 @@ public class ChargerBehaviour : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawRay(frontFeet.position, frontFeet.right * hitDistance);
-        Gizmos.DrawRay(frontFeet.position, -Vector3.up * hitDistance * 3f);
+        Gizmos.DrawRay(frontFeet.position, frontFeet.right * _hitDistance);
+        Gizmos.DrawRay(frontFeet.position, -Vector3.up * _hitDistance * 3f);
     }
 }
